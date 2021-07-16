fe8231a3dfa3cd0c0673e47e8052ea74c10c4db9
Marcus Lilja <marcus.t.lilja@gmail.com>
2021-07-16 20:58:04 +0200

    Update Main.cpp

--- src/My_SFML_Project/Main.cpp
+++ src/My_SFML_Project/Main.cpp
@@ -5,37 +5,37 @@ int main()
 {
 	const std::string title("My SFML Project");
 
-	sf::RenderWindow window; // (sf::VideoMode(1920, 1080), title, sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode(1280, 720), title, sf::Style::Default);
 
 	sf::Clock timer;
 
-	//while(window.isOpen())
-	//{
-	//	const float dt = timer.restart().asSeconds();
+	while(window.isOpen())
+	{
+		const float dt = timer.restart().asSeconds();
 
-	//	// Update objects here
+		// Update objects here
 
-	//	sf::Event event;
+		sf::Event event;
 
-	//	while(window.pollEvent(event))
-	//	{
-	//		switch (event.type)
-	//		{
-	//		case sf::Event::EventType::Closed:
-	//			window.close();
-	//			break;
+		while(window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::EventType::Closed:
+				window.close();
+				break;
 
-	//		default:
-	//			break;
-	//		}
-	//	}
+			default:
+				break;
+			}
+		}
 
-	//	window.clear(sf::Color());
-	//	// Draw stuff here!
-	//	window.display();
+		window.clear(sf::Color());
+		// Draw stuff here!
+		window.display();
 
-	//	//window.setTitle(title + std::string("\tFPS: ") + std::to_string(1.0f / dt));
-	//}
+		window.setTitle(title + std::string("\. FPS: ") + std::to_string(1.0f / dt));
+	}
 
 	return 0;
 }
